650d457d6175b69505cc129360f6fbf327c8e4c2
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-16 07:33:36 +0000

    delete

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a1d9ec3bf8db558f53443ab3623b04a21399eb89247e4cd48cee43920f96ad3e1ef79526ea2fdd6848bce8ee16bb9f8f0968cd074f54d8110215c4e3689a47cd'
